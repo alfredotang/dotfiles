cc4e930571dfae83986d82c007d4c3cc8cee8038
alfredotang <asdzxcca0617@gmail.com>
2021-05-12 16:32:00 +0800

    feat(install.sh): add test shell script

---
+++ macOS/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+echo -e "\n${COLOR_PURPLE}Test${COLOR_NONE}"
+echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
