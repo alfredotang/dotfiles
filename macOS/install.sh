472106c9887e5c0881d9f23cc4a3cd2937aed104
Alfredo Tang <asdzxcca0617@gmail.com>
2024-12-25 23:27:22 +0800

    feat: add git alias fetch --all --prune

--- macOS/install.sh
+++ macOS/install.sh
@@ -12,23 +12,22 @@ COLOR_NONE="\033[0m"
 # ========= utilites function ===========
 
 function print_out {
-    printf "\n${COLOR_BLUE}info ${COLOR_NONE}canel\n"
-    exit
+	printf "\n${COLOR_BLUE}info ${COLOR_NONE}canel\n"
+	exit
 }
 
 function success {
-    printf "\n ${COLOR_GREEN}success ${COLOR_NONE}$1\n"
+	printf "\n ${COLOR_GREEN}success ${COLOR_NONE}$1\n"
 }
 
 function error {
-    printf "\n${COLOR_RED}error ${COLOR_NONE}$1\n"
-    exit
+	printf "\n${COLOR_RED}error ${COLOR_NONE}$1\n"
+	exit
 }
 
-
 function print_step {
-    printf "\n${COLOR_PURPLE}step $1/$len  ${COLOR_YELLOW}$2"
-    printf "\n${COLOR_GRAY}=====================================${COLOR_NONE}\n"
+	printf "\n${COLOR_PURPLE}step $1/$len  ${COLOR_YELLOW}$2"
+	printf "\n${COLOR_GRAY}=====================================${COLOR_NONE}\n"
 }
 
 trap print_out SIGTERM SIGINT SIGHUP
@@ -37,97 +36,97 @@ trap print_out SIGTERM SIGINT SIGHUP
 
 # Install homebrew
 function install_homebrew {
-    title="Install Homebrew"
-    print_step $1 "$title"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	title="Install Homebrew"
+	print_step $1 "$title"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    success "$title"
+	success "$title"
 }
 
 # Install APP by homebrew
 function install_homebrew_dependencies {
-    title="Install Homebrew dependencies"
-    print_step $1 "$title"
-    curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/main/macOS/Brewfile > /tmp/Brewfile  
-    brew bundle --file /tmp/Brewfile  
-    rm /tmp/Brewfile
+	title="Install Homebrew dependencies"
+	print_step $1 "$title"
+	curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/main/macOS/Brewfile >/tmp/Brewfile
+	brew bundle --file /tmp/Brewfile
+	rm /tmp/Brewfile
 
-    success "$title"
+	success "$title"
 }
 
-# Setting zsh 
+# Setting zsh
 function configuare_zsh {
-    title="configuare zsh"
-    print_step $1 "$title"
-    curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/master/macOS/.zshrc > ~/.zshrc
+	title="configuare zsh"
+	print_step $1 "$title"
+	curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/master/macOS/.zshrc >~/.zshrc
 
-    success "$title"
+	success "$title"
 }
 
 # Setting zsh theme by powerlevel10k
 function configuare_powerlevel10k {
-    title="configuare powerlevel10k"
-    print_step $1 "$title"
-    curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/master/macOS/.p10k.zsh > ~/.p10k.zsh
+	title="configuare powerlevel10k"
+	print_step $1 "$title"
+	curl -fsSL https://raw.githubusercontent.com/alfredotang/dotfiles/master/macOS/.p10k.zsh >~/.p10k.zsh
 
-    success "$title"
+	success "$title"
 }
 
 # Setup zsh to deafault shell
 function setup_default_use_zsh {
-    title="setup default use zsh"
-    print_step $1 "$title"
-    hsh -s /bin/zsh
-    
-    success "$title"
+	title="setup default use zsh"
+	print_step $1 "$title"
+	hsh -s /bin/zsh
+
+	success "$title"
 }
 
 # To enable global key-repeat
 # If you're using vim's j k l h as your cursor movement keys
 # not having this setting enabled could be problematic when navigating.
 function disable_macos_press_and_hold {
-    title="enable global key-repeat"
-    print_step $1 "$title"
-    defaults write -g ApplePressAndHoldEnabled -bool false
+	title="enable global key-repeat"
+	print_step $1 "$title"
+	defaults write -g ApplePressAndHoldEnabled -bool false
 
-    success "$title"
+	success "$title"
 }
 
 # setting git global config
 function setting_git_global_config {
-    title="setting git global config"
-    print_step $1 "$title"
-    git config --global alias.co checkout
-    git config --global alias.br branch
-    git config --global alias.cm "commit -m"
-    git config --global alias.st status
-    git config --global alias.hist "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-    git config --global core.ignorecase false
-    git config --global user.email "asdzxcca0617@gmail.com"
-    git config --global user.name "Alfredo Tang"
-    git config --global init.defaultBranch main
+	title="setting git global config"
+	print_step $1 "$title"
+	git config --global alias.co checkout
+	git config --global alias.br branch
+	git config --global alias.cm "commit -m"
+	git config --global alias.st status
+	git config --global alias.hist "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+	git config --global alias.fa "fetch --all --prune"
+	git config --global core.ignorecase false
+	git config --global user.email "asdzxcca0617@gmail.com"
+	git config --global user.name "Alfredo Tang"
+	git config --global init.defaultBranch main
 
-    success "$title"
+	success "$title"
 }
 
 install_steps=(
-    # "install_homebrew"
-    "install_homebrew_dependencies" 
-    "configuare_zsh" 
-    "configuare_powerlevel10k" 
-    "setup_default_use_zsh" 
-    "disable_macos_press_and_hold" 
-    "setting_git_global_config"
+	# "install_homebrew"
+	"install_homebrew_dependencies"
+	"configuare_zsh"
+	"configuare_powerlevel10k"
+	"setup_default_use_zsh"
+	"disable_macos_press_and_hold"
+	"setting_git_global_config"
 )
 
 len=${#install_steps[*]}
 
-
 # ================= main function ========================
 {
-    for step in ${!install_steps[@]}; do
-        ${install_steps[$step]} `expr $step + 1`
-    done
+	for step in ${!install_steps[@]}; do
+		${install_steps[$step]} $(expr $step + 1)
+	done
 } || {
-    error "Error" SIGTERM SIGINT SIGHUP
+	error "Error" SIGTERM SIGINT SIGHUP
 }
